b6b20230484b68ecfcd7364d9ef5f46a07b9c07f
photon-phalanx <manbuzhu005@hotmail.com>
2018-07-04 01:07:19 +0900

    socket初始化

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = new Koa();
 const bodyParser = require('koa-bodyparser')
 const router = require('koa-router')();
 const userRoutes = require('./user')
-
 app.use(bodyParser())
 
 // return json
@@ -15,6 +14,14 @@ app.use(async (ctx, next) => {
 router.use('/user', userRoutes.routes())
 
 app.use(router.routes());
+const server = require('http').Server(app.callback())
+const io = require('socket.io')(server)
 
-app.listen(9093);
+io.on('connection', function (socket) {
+  socket.on('sendmsg', function (data) {
+    io.emit('recvmsg', data)
+  })
+})
+
+server.listen(9093);
 console.log('server listen on port 9093')
--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import io from 'socket.io-client'
 // import { connect } from 'react-redux'
-// import { List, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile'
+import { List, InputItem} from 'antd-mobile'
+import { login } from '../../redux/user.redux'
 // import { login } from '../../redux/user.redux'
 
+const socket = io('ws://localhost:9093')
 class Chat extends React.Component {
   //
   // register = () => {
@@ -14,12 +17,42 @@ class Chat extends React.Component {
   //   this.props.login(this.props.state)
   // }
 
+  constructor (props) {
+    super(props)
+    this.state = {
+      text: '',
+      msg: []
+    }
+  }
+  handleSubmit = () => {
+    socket.emit('sendmsg', {text: this.state.text})
+    this.setState({
+      text: ''
+    })
+  }
+  componentDidMount () {
+    socket.on('recvmsg', (data) => {
+      console.log(data)
+      this.setState({
+        msg: [...this.state.msg, data.text]
+      })
+    })
+  }
   render () {
     return (
       <div>
-        chat with user:{this.props.match.params.user}
+        {this.state.msg.map((v, index) => {
+          return <p key={index}>{v}</p>
+        })}
+        <div className="stick-footer">
+          <List>
+            <InputItem placeholder="请输入" value={this.state.text}
+                       onChange={text => {this.setState({text})}}
+                       extra={<span onClick={() => this.handleSubmit()}>发送</span>}
+            >信息</InputItem>
+          </List>
+        </div>
       </div>
-
     )
   }
 }
